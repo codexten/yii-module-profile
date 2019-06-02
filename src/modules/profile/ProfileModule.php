2d3e7bafdab5d5695d38bdb5992af5af9900e6d2
Jomon Johnson <jomon.entero@gmail.com>
2019-06-02 20:10:38 +0530

    init

---
+++ src/modules/profile/ProfileModule.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace codexten\yii\modules\profile;
+
+use yii\base\Module;
+
+class ProfileModule extends Module
+{
+
+}
---
+++ src/modules/profile/controllers/ProfileController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace codexten\yii\modules\profile\controllers;
+
+use codexten\yii\web\Controller;
+
+class ProfileController extends Controller
+{
+
+}
